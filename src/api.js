0e7b48b0eaabfc5964fa2162ee6a5910cf601d34
wwwwwmw <william.manwai@gmail.com>
2023-05-23 12:46:35 +0800

    shittymas

--- src/api.js
+++ src/api.js
@@ -1,4 +1,10 @@
-const openai = require('./openai');
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export async function fetchLatestLegCoData() {
   const response = await fetch('https://app4.legco.gov.hk/mapi/tc/api/LASS/getListMember');
@@ -9,8 +15,6 @@ export async function fetchLatestLegCoData() {
   return `There are ${totalMembers} members in the Legislative Council of the Hong Kong Special Administrative Region. The current members include: ${members}`;
 }
 
-
-
 export async function fetchGPT4Response(prompt, legCoContext) {
   const language = prompt.match(/[\u4e00-\u9fff]/) ? 'zh' : 'en';
 
@@ -20,18 +24,16 @@ export async function fetchGPT4Response(prompt, legCoContext) {
   };
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${promptDescription[language]}${legCoContext}\n\nUser: ${prompt}`,
-      temperature: 0.9,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "system", content: `${promptDescription[language]}${legCoContext}`},
+        {role: "user", content: prompt}
+      ],
       max_tokens: 1000,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: ["\nUser:"],
     });
 
-    return completion.data.choices[0].text.trim();
+    return completion.data.choices[0].message.content;
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
@@ -44,3 +46,44 @@ export async function fetchGPT4Response(prompt, legCoContext) {
 }
 
 
+
+
+export async function fetchLegCoSchedule() {
+  const response = await fetch('https://www.legco.gov.hk/xml/schedule.xml');
+  const xmlData = await response.text();
+  
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+  
+  const scheduleData = extractScheduleInfo(xmlDoc);
+
+  return scheduleData;
+}
+
+function extractScheduleInfo(xmlDoc) {
+  const detailSchedules = xmlDoc.getElementsByTagName('DetailSchedule');
+  const scheduleInfoArray = [];
+
+  for (let i = 0; i < detailSchedules.length; i++) {
+    const scheduleNode = detailSchedules[i];
+
+    const id = scheduleNode.getElementsByTagName('ID')[0]?.textContent;
+    const date = scheduleNode.getElementsByTagName('Date')[0]?.textContent;
+    const time = scheduleNode.getElementsByTagName('Time')[0]?.textContent;
+    const venueEN = scheduleNode.getElementsByTagName('EN')[0]?.textContent;
+    const venueCH = scheduleNode.getElementsByTagName('CH')[0]?.textContent;
+    const subjectEN = scheduleNode.getElementsByTagName('EN')[1]?.textContent;
+    const subjectCH = scheduleNode.getElementsByTagName('CH')[1]?.textContent;
+    const enUrl = scheduleNode.getElementsByTagName('En_Url')[0]?.textContent;
+    const chUrl = scheduleNode.getElementsByTagName('Ch_Url')[0]?.textContent;
+
+    const scheduleInfo = {
+      id, date, time, venueEN, venueCH, subjectEN, subjectCH, enUrl, chUrl
+    };
+
+    scheduleInfoArray.push(scheduleInfo);
+    
+  }
+  console.log(scheduleInfoArray)
+  return scheduleInfoArray;
+}
--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { fetchGPT4Response, fetchLatestLegCoData } from '../api';
+import { fetchGPT4Response, fetchLatestLegCoData, fetchLegCoSchedule } from '../api';
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
+// import { BeakerIcon } from '@heroicons/react/24/solid'
 import './Chatbot.css';
+
 function Chatbot() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
   const [legCoContext, setLegCoContext] = useState('');
+  const [legCoSchedule, setLegCoSchedule] = useState([]);
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
@@ -13,14 +17,16 @@ function Chatbot() {
 
   useEffect(scrollToBottom, [messages]);
 
- useEffect(() => {
-  const fetchData = async () => {
-    const context = await fetchLatestLegCoData();
-    setLegCoContext(context);
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const context = await fetchLatestLegCoData();
+      const schedule = await fetchLegCoSchedule();
+      setLegCoContext(context);
+      setLegCoSchedule(schedule);
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,68 +35,117 @@ function Chatbot() {
     setMessages([...messages, { text: input, sender: 'user' }]);
     setInput('');
 
-    const response = await fetchGPT4Response(input, legCoContext);
+    // We need to check if the user is asking for schedule information before asking the AI
+    let response = '';
+    if (input.toLowerCase().includes('schedule')) {
+      // The user asked for schedule information
+      // Let's format the schedule information into a user-readable format before sending it
+      response = formatScheduleInfo(legCoSchedule);
+    } else {
+      // The user didn't ask for schedule information
+      // Ask the AI as normal
+      response = await fetchGPT4Response(input, legCoContext);
+    }
+    
     setMessages((prevMessages) => [
       ...prevMessages,
       { text: response, sender: 'ai' },
     ]);
   };
 
+  const formatScheduleInfo = (schedule) => {
+  let formattedSchedule = 'Here is the upcoming LegCo meeting schedule:\n';
+  
+  schedule.forEach((meeting, index) => {
+    formattedSchedule += `\nMeeting ${index + 1}: \n\n`;
+
+    if(meeting.date){
+      formattedSchedule += `Date: ${meeting.date} \n\n`;
+    }
+
+    if(meeting.time){
+      formattedSchedule += `Time: ${meeting.time} \n\n`;
+    }
+
+    if(meeting.venueEN){
+      formattedSchedule += `Venue: ${meeting.venueEN} \n\n`;
+    }
+
+    if(meeting.subjectEN){
+      formattedSchedule += `Subject: ${meeting.subjectEN} \n\n`;
+    }
+
+    if(meeting.enUrl){
+      formattedSchedule += `More info: ${meeting.enUrl}\n\n`;
+    }
+  });
+  
+  return formattedSchedule;
+};
+
+
+
+
   const inputWordCount = input.split('').length;
 
-  return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 w-full sm:max-w-6xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white mx-4 md:mx-auto shadow rounded-3xl sm:p-10 w-full sm:w-11/12 lg:w-4/5 xl:w-3/4 2xl:w-2/3">
-          <div className="max-w-md mx-auto">
-            <div className="overflow-y-auto h-64">
-              {messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={`${
-                    message.sender === 'user'
-                      ? 'flex justify-end'
-                      : 'flex justify-start'
-                  }`}
-                >
-                  <div
-                    className={`p-2 rounded-md mb-2 ${
-                      message.sender === 'user'
-                        ? 'bg-blue-500 text-white'
-                        : 'bg-gray-200'
-                    }`}
-                  >
-                    {message.text}
-                  </div>
-                </div>
-              ))}
-              <div ref={messagesEndRef} />
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="relative">
-                <textarea
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-                  className="w-full focus:outline-none focus:ring-2 focus:ring-blue-400 border border-blue-300 rounded-md p-2 mt-4"
-                  rows="3"
-                  maxLength="4096"
-                />
-                <div className="absolute bottom-0 right-0 mr-2 mb-1 text-sm text-gray-600">
-                  {inputWordCount}
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 focus:outline-none text-white font-semibold rounded-lg px-4 py-3 mt-6"
-              >
-                Send
-              </button>
-            </form>
+    return (
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="min-w-[600] shadow-2xl bg-white max-w-md mx-auto rounded-xl overflow-hidden z-20 ">
+        <div className="h-20 bg-indigo-500 flex items-center justify-between px-5">
+          <div className="flex items-center">
+            <div className="rounded-full bg-indigo-400 w-10 h-10"></div>
+            <div className="ml-3 text-white">LegCo ChatBot</div>
           </div>
         </div>
+        <div className="h-96 overflow-y-scroll bg-gray-50 px-5 py-7">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`${
+                message.sender === 'user'
+                  ? 'flex justify-end'
+                  : 'flex justify-start'
+              }`}
+            >
+              <div
+                className={`p-2 rounded-lg mb-2 ${
+                  message.sender === 'user'
+                    ? 'bg-indigo-500 text-white'
+                    : 'bg-gray-200'
+                }`}
+              >
+                {message.sender === 'user' ? (
+                  message.text
+                ) : (
+                  <span dangerouslySetInnerHTML={{ __html: message.text.replace(/\n/g, '<br />') }} />
+                )}
+              </div>
+            </div>
+          ))}
+
+          <div ref={messagesEndRef} />
+        </div>
+        <form onSubmit={handleSubmit} className="border-t-2 border-indigo-200">
+          <div className="flex items-center p-4">
+            <textarea
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full focus:outline-none focus:ring-2 focus:ring-blue-400 border border-blue-300 rounded-md p-2 mr-4"
+              rows="1"
+              maxLength="4096"
+            />
+            <button
+              type="submit"
+              className="bg-indigo-500 hover:bg-indigo-600 rounded-full w-10 h-10 flex items-center justify-center text-white"
+            >
+              <PaperAirplaneIcon />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
 export default Chatbot;
+
